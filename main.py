8ac47c05dbbbe4340d4b298df9efd4653e4f1531
daevidvo <itsdavidvo@gmail.com>
2023-04-17 21:16:49 -0700

    imported libraries

---
+++ main.py
@@ -0,0 +1,13 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# makes numpy nums easier to read
+np.set_printoptions(precision=3, suppress=True)
+
+print(tf.__version__)
+
